75c4466921f216d5a9cf7cf598b826d34b7abb47
Daniel Flynn <daniel@digitalattic.com>
2019-01-02 12:39:10 -0800

    test

---
+++ bin.js
@@ -0,0 +1,26 @@
+const { spawn } = require('child_process');
+
+function check() {
+	try {
+		let glob = require('glob');
+		console.log('glob found');
+		return true;
+	}
+	catch (e) {
+		console.log('No glob found');
+		setTimeout(check, 1000);
+		return false;
+	}
+}
+
+if (!check()) {
+	let child = spawn('npm.cmd', ['i', 'glob']);
+
+	child.on('close', () => {
+		console.log('npm install finished');
+		setTimeout(function() {
+			console.log('quitting');
+			process.exit();
+		}, 3000);
+	});
+}
