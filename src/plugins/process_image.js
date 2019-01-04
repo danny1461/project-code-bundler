2b162a99f9c6735b9bdca0e48b8710895f27a0c7
Daniel Flynn <daniel@digitalattic.com>
2019-01-04 08:39:36 -0800

    feat: images now compress

--- src/plugins/process_image.js
+++ src/plugins/process_image.js
@@ -1,29 +1,53 @@
+const fs = require('fs');
+const path = require('path');
+const promisify = require('micro-promisify');
 const loadTaskDeps = require('../utils/loadTaskDeps');
+const log = require('../utils/log');
+const stopWatch = require('../utils/stopWatch');
 
 module.exports = function(watcher) {
 	watcher.registerExt('jpg jpeg png');
 
-	function doWork(file, ext) {
-		console.log(`Process ${ext.toUpperCase()} File: ${file}`);
+	const expiration = {};
 
-		return new Promise((resolve) => {
-			setTimeout(() => {
-				console.log('done');
-				resolve();
-			}, 5000);
+	async function doWork(file, plugin, imagemin) {
+		if (expiration[file]) {
+			return;
+		}
+
+		log(`Compressing image file:`);
+		log(`{{cyan:${file}}}`);
+
+		stopWatch.start('image_file');
+
+		let buffer = await promisify(fs.readFile)(file),
+			dataLen = buffer.length;
+		buffer = await imagemin.buffer(buffer, {
+			plugins: [plugin]
 		});
+
+		if (buffer.length < dataLen) {
+			await promisify(fs.writeFile)(file, buffer);
+
+			expiration[file] = true;
+			setTimeout(() => {
+				delete expiration[file];
+			}, 500);
+		}
+
+		log(`{{magenta:Task completed in ${stopWatch.end('image_file')}ms}}`);
 	}
 
 	// Primary work
 	watcher.on('add_jpg add_jpeg change_jpg change_jpeg', ({file, ext}) => {
 		return loadTaskDeps(['imagemin', 'imagemin-jpegtran']).then((libs) => {
-			return doWork(file, ext);
+			return doWork(file, libs.imageminJpegtran(), libs.imagemin);
 		});
 	});
 
 	watcher.on('add_png change_png', ({file, ext}) => {
 		return loadTaskDeps(['imagemin', 'imagemin-pngquant']).then((libs) => {
-			return doWork(file, ext);
+			return doWork(file, libs.imageminPngquant(), libs.imagemin);
 		});
 	});
 };
