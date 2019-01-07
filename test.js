79ca03a60647cdde7e0c50621aa2e2a2f9405e27
Daniel Flynn <daniel@digitalattic.com>
2019-01-07 12:24:58 -0800

    chore: bumped version

--- test.js
+++ test.js
@@ -1,34 +1,8 @@
-const Watcher = require('./');
+process.chdir('D:\\Devel\\PHP\\htdocs\\wordpress');
 
-process.chdir('C:\\Users\\danny\\Desktop\\x');
 global.cwd = process.cwd();
 
-function usage() {
-	let version = require('./package.json').version;
-	console.log(`
-Project Code Bundler v${version}
-
-bundler [[-args]]
--fsevents    The watcher will use a native watcher for your file system
--no-notify   The bundlers will not emit OS notifications on success/fail
-`);
-
-	process.exit();
-}
-
-let options = {};
-for (let i = 2; i < process.argv.length; i++) {
-	let arg = process.argv[i];
-	if (arg[0] != '-' || arg == '-?') {
-		usage();
-	}
-
-	arg = arg.substr(1).replace(/-[a-z]/gi, (str) => {
-		return str[1].toUpperCase();
-	});
-
-	options[arg] = true;
-}
+const Watcher = require('./');
 
 let watcher = new Watcher(options);
 watcher.start();
